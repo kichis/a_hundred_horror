f32023a974a6febd537395b70616677765be5974
kichis <nk.wisteira2@gmail.com>
2021-03-05 10:24:20 +0800

    バリデーション後のsignin_act.phpへの遷移、db登録後のsession変数をからする処理完成

--- signin_act.php
+++ signin_act.php
@@ -5,20 +5,12 @@ session_start();
 require("db_connection.php");
 include("funcs.php");
 
-$uname = $_POST["uname"];
-$email = $_POST["email"];
-$confemail = $_POST["confirmEmail"];
-$passw = password_hash($_POST["passw"], PASSWORD_DEFAULT);
-
-$_SESSION["signinErrorMsg"] = "";
-
-
+$uname = $_SESSION["uname"];
+$email = $_SESSION["email"];
+$passw = password_hash($_SESSION["passw"], PASSWORD_DEFAULT);
 
 // dbへ接続
 $pdo = db_conn();
-
-
-
 
 // データ登録SQL作成
 $sql = "INSERT INTO users(user_name, email, passw) VALUES(:user_name, :email, :passw)";
@@ -32,12 +24,7 @@ $status = $stmt->execute(); //実行
 if($status==false){
   sql_error();
 }else{
+  $_SESSION = array();
   redirect("login.php");
 }
-
-// signinErrorCode
-// 1: Emailが確認用Emailと異なる = "２つのEmailの入力が異なっています"
-// 2: 同じEmailがすでに登録がある = "このEmailアドレスは使用できません(すでに登録があります)"
-// 3: 同じuser_nameがすでに登録がある = "このお名前は使用できません(すでに登録があります)"
-// 4: 
 ?>

9bc4ca6f61098a0300084987e39ba4f9371f8075
kichis <nk.wisteira2@gmail.com>
2021-03-13 00:48:47 +0800

    story.phpでのコメント投稿機能の実装(簡易なバリデーションとダメパターンでは入力文の再表示)

--- story.php
+++ story.php
@@ -68,56 +68,55 @@ $comments = getComment($pdo, $id);
     include($menu());
     ?>
     
-    <div id="mainImage">
-        <!-- <div id="gray">a</div> -->
+    <div id="mainImage_2">
             <div id="story_main" class="mx-auto">
                 <!-- ストーリーメイン -->
                 <p>#<?=$r["story_id"]?></p>
                 <h4 class="text-primary"><?= h($r["title"])?></h4>   
-                <p>[ &nbsp; 語り手：<?= $r["user"];?> &nbsp; / &nbsp; <?= $r["date"];?> &nbsp; ]</p>
-                <p id="story_content"><?= $r["content"];?></p>
+                <p>[ &nbsp; 語り手：<?= h($r["user"]);?> &nbsp; / &nbsp; <?= $r["date"];?> &nbsp; ]</p>
+                <p id="story_content"><?= h($r["content"])?></p>
                 <!-- 怖ボタン -->
                 <form mehod='get' action='add_horror.php'>
                     <p class="d-inline-flex mr-3"><i class="fas fa-ghost mr-2 my-auto"></i><?= $r["horror"]?></p>
                     <?php
                     if($_SESSION["user_status"]==1||$_SESSION["user_status"]==2){
                         $isPushed = hasPushed($pdo, $id, $_SESSION["user_id"]);
-                        echo "<button type='submit' name='story_id' value='".$r["story_id"]."' 
+                        echo "<button type='submit' name='story_id' value='{$r["story_id"]}' 
                                 class='btn btn-sm bg-dark text-white d-inline-flex'";
                             if($isPushed==false){
                                 echo ">怖！</button>";
                             }else{
                                 echo "disabled>怖！済み</button>";
                             }
-                        echo    "<input type='hidden' name='user_id' value='".$_SESSION['user_id']."'>";
+                        echo    "<input type='hidden' name='user_id' value='{$_SESSION['user_id']}'>";
                     }
                     ?>
                 </form>
-                <!-- コメントエリア -->
+                <!-- コメントエリア(コメントは1万文字まで) -->
                 <div class="mt-3">
                     <p class=""><b>comment</b></p>
                     <?php
                     for($i=0 ; $i < count($comments); $i++){
                         $comm = $comments[$i];
-                        echo "<p class='mb-1 ml-3'> {$comm["user"]} ( {$comm["date"]} )</p>";
-                        echo "<p class='comm_area d-inline-flex py-1 px-2 mb-3 ml-3'>{$comm["comment"]}</p><br>";
+                        echo "<p class='mb-1 ml-3'>" . h($comm["user"]) . " ( {$comm["date"]} )</p>";
+                        echo "<p class='comm_area d-inline-flex py-1 px-2 mb-3 ml-3'>" . h($comm["comment"]) . "</p><br>";
                     }
                     if($_SESSION["user_status"]==1||$_SESSION["user_status"]==2){
-                        echo "<form method='post' action='comm_act.php'>
-                        <textarea name='comment' rows='3' class='form-control w-50' style='float:left'></textarea>
-                        <button type='submit' class='btn btn-sm bg-dark text-white ml-3' name='id' value='".$r["id"]."'>感想</button>
-                        </form>";
+                        echo "<form method='post' action='post_comment.php'>
+                                <textarea name='comment' rows='3' class='form-control w-50 d-inline-flex align-bottom mt-4' placeholder='コメントする(10,000字以内)'>{$_SESSION["comment"]}</textarea>
+                                <button type='submit' class='btn btn-sm bg-dark text-white ml-3' name='story_id' value='{$r["story_id"]}'>投稿する</button>
+                            </form>";
                     }
+                    $_SESSION["comment"] = "";
                     ?>
                 </div>
             </div>
         
-         <!-- </div> -->
-            <div id="story_2">
+            <div id="story_bottom" class="mx-auto">
                 <a href="home.php">← HOMEへ戻る</a>
             </div>
     </div>
-
+    
     <?php include("copyright.php"); ?>
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
